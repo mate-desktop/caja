66548d792796eb618f7abae9cf768a3e4fecaa2c
Nelson Marques <nmo.marques@gmail.com>
2012-10-11 02:55:25 +0200

    fix compiler warnings: assignment makes integer from pointer without a cast

--- libcaja-private/caja-ui-utilities.c
+++ libcaja-private/caja-ui-utilities.c
@@ -263,7 +263,7 @@ caja_get_thumbnail_frame (void)
 
     if (thumbnail_frame == NULL)
     {
-        image_path = caja_pixmap_file ("thumbnail_frame.png");
+        *image_path = caja_pixmap_file ("thumbnail_frame.png");
         if (image_path != NULL)
         {
             thumbnail_frame = gdk_pixbuf_new_from_file (image_path, NULL);
@@ -304,7 +304,7 @@ caja_ui_frame_image (GdkPixbuf **pixbuf)
     right_offset = CAJA_THUMBNAIL_FRAME_RIGHT;
     bottom_offset = CAJA_THUMBNAIL_FRAME_BOTTOM;
 
-    pixbuf_with_frame = eel_embed_image_in_frame
+    *pixbuf_with_frame = eel_embed_image_in_frame
         (*pixbuf, frame,
          left_offset, top_offset, right_offset, bottom_offset);
     g_object_unref (*pixbuf);
