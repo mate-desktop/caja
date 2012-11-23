mate-desktop/caja

commit 7a42b9b076d6b831c89bd0b9e996368c1c95aef3

[all] use g_list_free() and g_strcmp0 instead of eel functions
 Was: general: use g_list_free_full() instead of eel functions
 http://git.gnome.org/browse/nautilus/commit/?id=5e669515fd7f760382e6b7aa1449734a35a2d7f4
 .
 Instead of g_list_free_full(), we use g_list_foreach and g_list_free() to avoid
 unnecessary glib dependency bump to 2.28

src/file-manager/fm-directory-view.c
@@ -1025,7 +1025,8 @@ delete_selected_files (FMDirectoryView *view)
 
 	caja_file_operations_delete (locations, fm_directory_view_get_containing_window (view), NULL, NULL);
 
-	eel_g_object_list_free (locations);
+	g_list_foreach(locations, (GFunc) g_object_unref, NULL);
+	g_list_free(locations);
         caja_file_list_free (selection);
 }
 
@@ -1934,7 +1935,8 @@ fm_directory_view_set_selection_locations (CajaView *caja_view,
 		/* If we are still loading, set the list of pending URIs instead.
 		 * done_loading() will eventually select the pending URIs and reveal them.
 		 */
-		eel_g_object_list_free (view->details->pending_locations_selected);
+		g_list_foreach(view->details->pending_locations_selected, (GFunc) g_object_unref, NULL);
+		g_list_free(view->details->pending_locations_selected);
 		view->details->pending_locations_selected =
 			eel_g_object_list_copy (selection_locations);
 	}
@@ -2548,7 +2550,8 @@ done_loading (FMDirectoryView *view,
 				fm_directory_view_reveal_selection (view);
 			}
 		}
-		eel_g_object_list_free (locations_selected);
+		g_list_foreach(locations_selected, (GFunc) g_object_unref, NULL);
+		g_list_free(locations_selected);
 		fm_directory_view_display_selection_info (view);
 	}
 
@@ -3790,7 +3793,8 @@ fm_directory_view_create_links_for_files (FMDirectoryView *view, GList *files,
 	caja_file_operations_copy_move (uris, relative_item_points, dir_uri, GDK_ACTION_LINK,
 					    GTK_WIDGET (view), copy_move_done_callback, copy_move_done_data);
 	g_free (dir_uri);
-	eel_g_list_free_deep (uris);
+	g_list_foreach(uris, (GFunc) g_free, NULL);
+	g_list_free(uris);
 }
 
 static void
@@ -3822,7 +3826,8 @@ fm_directory_view_duplicate_selection (FMDirectoryView *view, GList *files,
         copy_move_done_data = pre_copy_move (view);
 	caja_file_operations_copy_move (uris, relative_item_points, NULL, GDK_ACTION_COPY,
 		GTK_WIDGET (view), copy_move_done_callback, copy_move_done_data);
-	eel_g_list_free_deep (uris);
+	g_list_foreach(uris, (GFunc) g_free, NULL);
+	g_list_free(uris);
 }
 
 /* special_link_in_selection
@@ -3926,7 +3931,8 @@ trash_or_delete_files (GtkWindow *parent_window,
 						  parent_window,
 						  (CajaDeleteCallback) trash_or_delete_done_cb,
 						  view);
-	eel_g_object_list_free (locations);
+	g_list_foreach(locations, (GFunc) g_object_unref, NULL);
+	g_list_free(locations);
 }
 
 static gboolean
@@ -4675,7 +4681,8 @@ reset_open_with_menu (FMDirectoryView *view, GList *selection)
 						   index,
 						   menu_path, popup_path, submenu_visible);
 	}
-	eel_g_object_list_free (applications);
+	g_list_foreach(applications, (GFunc) g_object_unref, NULL);
+	g_list_free(applications);
 	if (default_app != NULL) {
 		g_object_unref (default_app);
 	}
@@ -5970,7 +5977,8 @@ move_copy_selection_to_location (FMDirectoryView *view,
 					   0, 0,
 					   view);
 
-	eel_g_list_free_deep (uris);
+	g_list_foreach(uris, (GFunc) g_free, NULL);
+	g_list_free(uris);
 	caja_file_list_free (selection);
 }
 
@@ -6110,7 +6118,10 @@ paste_clipboard_data (FMDirectoryView *view,
 			gtk_clipboard_clear (caja_clipboard_get (GTK_WIDGET (view)));
 		}
 
-		eel_g_list_free_deep (item_uris);
+    		g_list_foreach(item_uris, (GFunc) g_free, NULL);
+    		g_list_free(item_uris);
+		g_list_foreach(item_uris, (GFunc) g_free, NULL);
+		g_list_free(item_uris);
 	}
 }
 
@@ -7104,7 +7115,8 @@ action_location_delete_callback (GtkAction *action,
 	caja_file_operations_delete (files, fm_directory_view_get_containing_window (view),
 					 NULL, NULL);
 
-	eel_g_object_list_free (files);
+	g_list_foreach(files, (GFunc) g_object_unref, NULL);
+	g_list_free(files);
 }
 
 static void
@@ -9787,7 +9799,8 @@ fm_directory_view_stop (FMDirectoryView *view)
 	view->details->old_added_files = NULL;
 	file_and_directory_list_free (view->details->old_changed_files);
 	view->details->old_changed_files = NULL;
-	eel_g_object_list_free (view->details->pending_locations_selected);
+	g_list_foreach(view->details->pending_locations_selected, (GFunc) g_object_unref, NULL);
+	g_list_free(view->details->pending_locations_selected);
 	view->details->pending_locations_selected = NULL;
 
 	if (view->details->model != NULL) {
@@ -10565,7 +10578,8 @@ fm_directory_view_handle_uri_list_drop (FMDirectoryView  *view,
 					   target_uri != NULL ? target_uri : container_uri,
 					   action, x, y, view);
 
-	eel_g_list_free_deep (real_uri_list);
+	g_list_foreach(real_uri_list, (GFunc) g_free, NULL);
+	g_list_free(real_uri_list);
 
 	if (points != NULL)
 		g_array_free (points, TRUE);

eel/eel-gdk-pixbuf-extensions.c
@@ -66,18 +66,6 @@ eel_gdk_pixbuf_list_ref (GList *pixbuf_list)
     g_list_foreach (pixbuf_list, (GFunc) g_object_ref, NULL);
 }
 
-/**
- * eel_gdk_pixbuf_list_free
- * @pixbuf_list: A list of GdkPixbuf objects.
- *
- * Unrefs all the pixbufs, then frees the list.
- **/
-void
-eel_gdk_pixbuf_list_free (GList *pixbuf_list)
-{
-    eel_g_list_free_deep_custom (pixbuf_list, (GFunc) g_object_unref, NULL);
-}
-
 GdkPixbuf *
 eel_gdk_pixbuf_load (const char *uri)
 {

eel/eel-gdk-pixbuf-extensions.h
@@ -43,8 +43,6 @@ typedef void (* EelPixbufLoadCallback) (GError         *error,
 
 /* Convenience functions for lists of GdkPixbuf objects. */
 void                 eel_gdk_pixbuf_list_ref                  (GList                 *pixbuf_list);
-void                 eel_gdk_pixbuf_list_unref                (GList                 *pixbuf_list);
-void                 eel_gdk_pixbuf_list_free                 (GList                 *pixbuf_list);
 
 
 /* Loading a GdkPixbuf with a URI. */

eel/eel-glib-extensions.c
@@ -376,63 +376,6 @@ eel_g_str_list_index (GList *str_list,
 }
 
 /**
- * eel_g_list_free_deep_custom
- *
- * Frees the elements of a list and then the list, using a custom free function.
- *
- * @list: List of elements that can be freed with the provided free function.
- * @element_free_func: function to call with the data pointer and user_data to free it.
- * @user_data: User data to pass to element_free_func
- **/
-void
-eel_g_list_free_deep_custom (GList *list, GFunc element_free_func, gpointer user_data)
-{
-    g_list_foreach (list, element_free_func, user_data);
-    g_list_free (list);
-}
-
-/**
- * eel_g_list_free_deep
- *
- * Frees the elements of a list and then the list.
- * @list: List of elements that can be freed with g_free.
- **/
-void
-eel_g_list_free_deep (GList *list)
-{
-    eel_g_list_free_deep_custom (list, (GFunc) g_free, NULL);
-}
-
-/**
- * eel_g_list_free_deep_custom
- *
- * Frees the elements of a list and then the list, using a custom free function.
- *
- * @list: List of elements that can be freed with the provided free function.
- * @element_free_func: function to call with the data pointer and user_data to free it.
- * @user_data: User data to pass to element_free_func
- **/
-void
-eel_g_slist_free_deep_custom (GSList *list, GFunc element_free_func, gpointer user_data)
-{
-    g_slist_foreach (list, element_free_func, user_data);
-    g_slist_free (list);
-}
-
-/**
- * eel_g_slist_free_deep
- *
- * Frees the elements of a list and then the list.
- * @list: List of elements that can be freed with g_free.
- **/
-void
-eel_g_slist_free_deep (GSList *list)
-{
-    eel_g_slist_free_deep_custom (list, (GFunc) g_free, NULL);
-}
-
-
-/**
  * eel_g_strv_find
  *
  * Get index of string in array of strings.
@@ -759,31 +702,6 @@ eel_g_object_list_ref (GList *list)
 {
     g_list_foreach (list, (GFunc) g_object_ref, NULL);
     return list;
-}
-
-/**
- * eel_g_object_list_unref
- *
- * Unref all the objects in a list.
- * @list: GList of objects.
- **/
-void
-eel_g_object_list_unref (GList *list)
-{
-    g_list_foreach (list, (GFunc) g_object_unref, NULL);
-}
-
-/**
- * eel_g_object_list_free
- *
- * Free a list of objects after unrefing them.
- * @list: GList of objects.
- **/
-void
-eel_g_object_list_free (GList *list)
-{
-    eel_g_object_list_unref (list);
-    g_list_free (list);
 }
 
 /**
@@ -1214,11 +1132,16 @@ eel_self_check_glib_extensions (void)
     EEL_CHECK_BOOLEAN_RESULT (eel_g_str_list_equal (compare_list_1, compare_list_4), FALSE);
     EEL_CHECK_BOOLEAN_RESULT (eel_g_str_list_equal (compare_list_1, compare_list_5), FALSE);
 
-    eel_g_list_free_deep (compare_list_1);
-    eel_g_list_free_deep (compare_list_2);
-    eel_g_list_free_deep (compare_list_3);
-    eel_g_list_free_deep (compare_list_4);
-    eel_g_list_free_deep (compare_list_5);
+    g_list_foreach (compare_list_1, (GFunc) g_free, NULL);
+    g_list_free(compare_list_1);
+    g_list_foreach (compare_list_2, (GFunc) g_free, NULL);
+    g_list_free(compare_list_2);
+    g_list_foreach (compare_list_3, (GFunc) g_free, NULL);
+    g_list_free(compare_list_3);
+    g_list_foreach (compare_list_4, (GFunc) g_free, NULL);
+    g_list_free(compare_list_4);
+    g_list_foreach (compare_list_5, (GFunc) g_free, NULL);
+    g_list_free(compare_list_5);
 
     /* eel_g_list_partition */
 

eel/eel-glib-extensions.h
@@ -54,18 +54,6 @@ GList *     eel_g_list_partition                        (GList                 *
         gpointer               user_data,
         GList                **removed);
 
-/* List functions for lists of g_free'able objects. */
-void        eel_g_list_free_deep                        (GList                 *list);
-void        eel_g_list_free_deep_custom                 (GList                 *list,
-        GFunc                  element_free_func,
-        gpointer               user_data);
-
-/* List functions for slists of g_free'able objects. */
-void        eel_g_slist_free_deep                       (GSList                *list);
-void        eel_g_slist_free_deep_custom                (GSList                *list,
-        GFunc                  element_free_func,
-        gpointer               user_data);
-
 /* List functions for lists of C strings. */
 gboolean    eel_g_str_list_equal                        (GList                 *str_list_a,
         GList                 *str_list_b);
@@ -76,8 +64,6 @@ int         eel_g_str_list_index                        (GList                 *
 
 /* List functions for lists of objects */
 GList *     eel_g_object_list_ref                       (GList                 *list);
-void        eel_g_object_list_unref                     (GList                 *list);
-void        eel_g_object_list_free                      (GList                 *list);
 GList *     eel_g_object_list_copy                      (GList                 *list);
 
 /* GHashTable functions */

libcaja-private/caja-autorun.c
@@ -33,7 +33,6 @@
 #include <gdk/gdkkeysyms.h>
 
 #include <eel/eel-glib-extensions.h>
-#include <eel/eel-stock-dialogs.h>
 
 #include "caja-icon-info.h"
 #include "caja-global-preferences.h"
@@ -652,7 +651,8 @@ caja_autorun_prepare_combo_box (GtkWidget *combo_box,
     {
         g_object_unref (default_app_info);
     }
-    eel_g_object_list_free (app_info_list);
+    g_list_foreach (app_info_list, (GFunc) g_object_unref, NULL);
+    g_list_free(app_info_list);
 
     gtk_combo_box_set_model (GTK_COMBO_BOX (combo_box), GTK_TREE_MODEL (list_store));
     g_object_unref (G_OBJECT (list_store));

libcaja-private/caja-bookmark.c
@@ -29,10 +29,10 @@
 #include <eel/eel-gdk-pixbuf-extensions.h>
 #include <eel/eel-gtk-extensions.h>
 #include <eel/eel-gtk-macros.h>
-#include <eel/eel-string.h>
 #include <eel/eel-vfs-extensions.h>
 #include <gtk/gtk.h>
 #include <gio/gio.h>
+#include <string.h>
 #include <libcaja-private/caja-file.h>
 #include <libcaja-private/caja-icon-names.h>
 
@@ -143,7 +143,7 @@ caja_bookmark_compare_with (gconstpointer a, gconstpointer b)
     bookmark_a = CAJA_BOOKMARK (a);
     bookmark_b = CAJA_BOOKMARK (b);
 
-    if (eel_strcmp (bookmark_a->details->name,
+    if (g_strcmp0 (bookmark_a->details->name,
                     bookmark_b->details->name) != 0)
     {
         return 1;

libcaja-private/caja-clipboard.c
@@ -33,7 +33,6 @@
 
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
-#include <eel/eel-glib-extensions.h>
 #include <string.h>
 
 typedef struct _TargetCallbackData TargetCallbackData;
@@ -662,31 +661,27 @@ caja_clipboard_clear_if_colliding_uris (GtkWidget *widget,
     collision = FALSE;
     data = gtk_clipboard_wait_for_contents (caja_clipboard_get (widget),
                                             copied_files_atom);
-    if (data == NULL)
-    {
+    if (data == NULL) {
         return;
     }
 
     clipboard_item_uris = caja_clipboard_get_uri_list_from_selection_data (data, NULL,
                           copied_files_atom);
 
-    for (l = (GList *) item_uris; l; l = l->next)
-    {
+    for (l = (GList *) item_uris; l; l = l->next) {
         if (g_list_find_custom ((GList *) item_uris, l->data,
-                                (GCompareFunc) g_strcmp0))
-        {
+                                (GCompareFunc) g_strcmp0)) {
             collision = TRUE;
             break;
         }
     }
 
-    if (collision)
-    {
+    if (collision) {
         gtk_clipboard_clear (caja_clipboard_get (widget));
     }
 
-    if (clipboard_item_uris)
-    {
-        eel_g_list_free_deep (clipboard_item_uris);
+    if (clipboard_item_uris) {
+        g_list_foreach (clipboard_item_uris, (GFunc) g_free, NULL);
+    	g_list_free(clipboard_item_uris);
     }
 }

commit c7862d8bc2f25b627aab3d51577feaabb023a70e

[directory-view] fix double-free regression from 7a42b9b0

src/file-manager/fm-directory-view.c
@@ -6120,8 +6120,6 @@ paste_clipboard_data (FMDirectoryView *view,
 
     		g_list_foreach(item_uris, (GFunc) g_free, NULL);
     		g_list_free(item_uris);
-		g_list_foreach(item_uris, (GFunc) g_free, NULL);
-		g_list_free(item_uris);
 	}
 }
 