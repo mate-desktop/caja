63324206cb0e487fc9ce7c17632c91fdcd92f4c1
Jasmine Hassan <jasmine.aura@gmail.com>
2012-12-25 06:24:12 +0200

    [icon-container] apply emblems directly on the pixbuf
    Do that before setting the pixbuf to the CajaIconCanvasItem,
    so the logic there can be removed.
    
    http://git.gnome.org/browse/nautilus/commit/?id=a728e7b830ebeccfba6d851a5b005033ba07189b
    http://git.gnome.org/browse/nautilus/commit/?id=79a9c6a36256d430459b4d2a7bc37e0d691c8e5a

--- libcaja-private/caja-icon-container.c
+++ libcaja-private/caja-icon-container.c
@@ -7651,13 +7651,15 @@ caja_icon_container_update_icon (CajaIconContainer *container,
     int n_attach_points;
     gboolean has_embedded_text_rect;
     GdkPixbuf *pixbuf;
-    GList *emblem_pixbufs;
+    GList *emblem_pixbufs, *l;
     char *editable_text, *additional_text;
     char *embedded_text;
     GdkRectangle embedded_text_rect;
     gboolean large_embedded_text;
     gboolean embedded_text_needs_loading;
     gboolean has_open_window;
+    GIcon *emblemed_icon;
+    GEmblem *emblem;
 
     if (icon == NULL)
     {
@@ -7695,14 +7697,43 @@ caja_icon_container_update_icon (CajaIconContainer *container,
                 large_embedded_text, &embedded_text_needs_loading,
                 &has_open_window);
 
-
     if (container->details->forced_icon_size > 0)
+    {
         pixbuf = caja_icon_info_get_pixbuf_at_size (icon_info, icon_size);
+    }
     else
+    {
         pixbuf = caja_icon_info_get_pixbuf (icon_info);
+    }
+
     caja_icon_info_get_attach_points (icon_info, &attach_points, &n_attach_points);
-    has_embedded_text_rect = caja_icon_info_get_embedded_rect (icon_info,
-                             &embedded_text_rect);
+    has_embedded_text_rect = caja_icon_info_get_embedded_rect (icon_info, &embedded_text_rect);
+
+    /* apply emblems */
+    if (emblem_pixbufs != NULL) {
+        l = emblem_pixbufs;
+
+        emblem = g_emblem_new (l->data);
+        emblemed_icon = g_emblemed_icon_new (G_ICON (pixbuf), emblem);
+        g_object_unref (emblem);
+
+        for (l = l->next; l != NULL; l = l->next) {
+            emblem = g_emblem_new (l->data);
+            g_emblemed_icon_add_emblem (G_EMBLEMED_ICON (emblemed_icon),
+                            emblem);
+            g_object_unref (emblem);
+        }
+
+        g_object_unref (icon_info);
+        g_object_unref (pixbuf);
+
+        icon_info = caja_icon_info_lookup (emblemed_icon, icon_size);
+        pixbuf = caja_icon_info_get_pixbuf_at_size (icon_info, icon_size);
+
+        g_object_unref (emblemed_icon);
+    }
+
+    g_object_unref (icon_info);
 
     if (has_embedded_text_rect && embedded_text_needs_loading)
     {
@@ -7736,7 +7767,6 @@ caja_icon_container_update_icon (CajaIconContainer *container,
 
     caja_icon_canvas_item_set_image (icon->item, pixbuf);
     caja_icon_canvas_item_set_attach_points (icon->item, attach_points, n_attach_points);
-    caja_icon_canvas_item_set_emblems (icon->item, emblem_pixbufs);
     caja_icon_canvas_item_set_embedded_text_rect (icon->item, &embedded_text_rect);
     caja_icon_canvas_item_set_embedded_text (icon->item, embedded_text);
 
@@ -7746,8 +7776,6 @@ caja_icon_container_update_icon (CajaIconContainer *container,
 
     g_free (editable_text);
     g_free (additional_text);
-
-    g_object_unref (icon_info);
 }
 
 static gboolean
