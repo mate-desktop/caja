a706a5994f0b704a3016416983dccce536ebe5f5
Stefano Karapetsas <stefano@karapetsas.com>
2013-07-25 15:11:46 +0200

    cut-n-paste-code: Add GTK3 support

--- cut-n-paste-code/libegg/eggsmclient-xsmp.c
+++ cut-n-paste-code/libegg/eggsmclient-xsmp.c
@@ -35,7 +35,11 @@
 #include <unistd.h>
 #include <X11/SM/SMlib.h>
 
+#include <gtk/gtk.h>
 #include <gdk/gdk.h>
+#if GTK_CHECK_VERSION (3, 0, 0)
+#include <gdk/gdkx.h>
+#endif
 
 #define EGG_TYPE_SM_CLIENT_XSMP            (egg_sm_client_xsmp_get_type ())
 #define EGG_SM_CLIENT_XSMP(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EGG_TYPE_SM_CLIENT_XSMP, EggSMClientXSMP))
@@ -368,7 +372,11 @@ sm_client_xsmp_startup (EggSMClient *client,
         free (ret_client_id);
 
         gdk_threads_enter ();
+#if GTK_CHECK_VERSION (3, 0, 0)
+        gdk_x11_set_sm_client_id (xsmp->client_id);
+#else
         gdk_set_sm_client_id (xsmp->client_id);
+#endif
         gdk_threads_leave ();
 
         g_debug ("Got client ID \"%s\"", xsmp->client_id);
