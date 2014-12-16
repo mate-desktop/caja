08bc0644ebc869ff1db320cc2bdfc4a471a6f9ac
infirit <infirit@gmail.com>
2014-12-16 13:54:43 +0100

    dbus-manager: add a skeleton of NautilusDBusManager

---
+++ libcaja-private/caja-dbus-manager.h
@@ -0,0 +1,10 @@
+
+#ifndef __CAJA_DBUS_MANAGER_H__
+#define __CAJA_DBUS_MANAGER_H__
+
+#include <glib-object.h>
+
+void caja_dbus_manager_start (void);
+void caja_dbus_manager_stop (void);
+
+#endif /* __CAJA_DBUS_MANAGER_H__ */
--- src/caja-application.c
+++ src/caja-application.c
@@ -66,6 +66,7 @@
 #include <eel/eel-stock-dialogs.h>
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
+#include <libcaja-private/caja-dbus-manager.h>
 #include <libcaja-private/caja-debug-log.h>
 #include <libcaja-private/caja-file-utilities.h>
 #include <libcaja-private/caja-global-preferences.h>
@@ -373,6 +374,8 @@ caja_application_finalize (GObject *object)
         g_object_unref (fdb_manager);
         fdb_manager = NULL;
     }
+
+    caja_dbus_manager_stop ();
 
     G_OBJECT_CLASS (caja_application_parent_class)->finalize (object);
 }
@@ -926,6 +929,8 @@ caja_application_startup (CajaApplication *application,
             g_free (accel_map_filename);
         }
         g_signal_connect (gtk_accel_map_get (), "changed", G_CALLBACK (queue_accel_map_save_callback), NULL);
+
+        caja_dbus_manager_start ();
     }
 }
 
