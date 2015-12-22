53f698436f2111bf8655ad9039c6a93948e10028
monsta <monsta@inbox.ru>
2015-12-22 17:01:40 +0300

    oops... fix build breakage introduced in last commit

--- cut-n-paste-code/libegg/eggsmclient-private.h
+++ cut-n-paste-code/libegg/eggsmclient-private.h
@@ -44,6 +44,7 @@ void      egg_sm_client_quit           (EggSMClient *client);
     GType        egg_sm_client_dbus_get_type (void);
     EggSMClient *egg_sm_client_dbus_new      (void);
 #endif
+#endif
 
 G_END_DECLS
 
