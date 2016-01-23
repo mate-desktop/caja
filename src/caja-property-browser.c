b058255bef4dab6ce19c6b056af2b32cd2e386a0
Wolfgang Ulbrich <chat-to-me@raveit.de>
2016-01-23 17:59:42 +0100

    GTK3 property-browser: expand the GtkScrolledWindow vertical

--- src/caja-property-browser.c
+++ src/caja-property-browser.c
@@ -2186,6 +2186,9 @@ caja_property_browser_update_contents (CajaPropertyBrowser *property_browser)
 
     /* allocate a new container, with a scrollwindow and viewport */
     property_browser->details->content_frame = gtk_scrolled_window_new (NULL, NULL);
+#if GTK_CHECK_VERSION (3, 0, 0)
+    gtk_widget_set_vexpand (property_browser->details->content_frame, TRUE);
+#endif
     viewport = gtk_viewport_new (NULL, NULL);
     gtk_widget_show(viewport);
     gtk_viewport_set_shadow_type(GTK_VIEWPORT(viewport), GTK_SHADOW_IN);
