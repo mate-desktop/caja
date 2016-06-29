64b18d03b45160b0af841ffa4422e5dc59c9b537
raveit65 <chat-to-me@raveit.de>
2016-06-29 11:33:23 +0200

    GTK+-3 pathbar: always show the sliders
    taken from:
    https://git.gnome.org/browse/nautilus/commit/src?id=6f6bfde

--- src/caja-pathbar.c
+++ src/caja-pathbar.c
@@ -588,7 +588,7 @@ caja_path_bar_get_preferred_width (GtkWidget *widget,
      */
     path_bar->slider_width = MIN (height * 2 / 3 + 5, height);
 
-    if (path_bar->button_list && path_bar->button_list->next != NULL) {
+    if (path_bar->button_list) {
     	*minimum += (path_bar->spacing + path_bar->slider_width) * 2;
     	*natural += (path_bar->spacing + path_bar->slider_width) * 2;
     }
@@ -719,7 +719,7 @@ caja_path_bar_size_allocate (GtkWidget     *widget,
     GtkRequisition child_requisition;
     GtkAllocation widget_allocation;
 
-    need_sliders = FALSE;
+    need_sliders = TRUE;
     up_slider_offset = 0;
     down_slider_offset = 0;
     path_bar = CAJA_PATH_BAR (widget);
