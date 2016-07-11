5968f6bb40c334a797175ebeeb946d76163ce288
raveit65 <chat-to-me@raveit.de>
2016-07-11 22:25:28 +0200

    Revert "GTK+-3 spatial-window: avoid deprecated gtk_vbox_new"
    This reverts commit d866d7a3a5b778cbd413eaa67ad69c80e9277c6b.
    This commit breaks desktop drawing,
    revert fixes https://github.com/mate-desktop/caja/issues/591

--- src/caja-spatial-window.c
+++ src/caja-spatial-window.c
@@ -77,7 +77,6 @@
 
 #if GTK_CHECK_VERSION (3, 0, 0)
 #define gtk_hbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL,Y)
-#define gtk_vbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_VERTICAL,Y)
 #endif
 
 struct _CajaSpatialWindowDetails
