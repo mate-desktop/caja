b51e8ea28cbe8cf3c90c1357a05e45489e396c50
lukefromdc <lukefromdc@hushmail.com>
2017-12-13 00:39:20 -0500

    places-sidebar: make sure the eject icon is rendered 16x16 Apply https://github.com/GNOME/nautilus/commit/679486ff6db16138027aa85c2aadbb9d272bc984 This is a correction to an earlier eject commit based on Nautilus that can cause the eject icon to render fuzzy in some themes https://bugzilla.gnome.org/show_bug.cgi?id=647570

--- src/caja-places-sidebar.c
+++ src/caja-places-sidebar.c
@@ -240,7 +240,7 @@ get_eject_icon (CajaPlacesSidebar *sidebar,
     GtkStyleContext *style;
 
     icon_theme = gtk_icon_theme_get_default ();
-    icon_size = caja_get_icon_size_for_stock_size (GTK_ICON_SIZE_SMALL_TOOLBAR);
+    icon_size = caja_get_icon_size_for_stock_size (GTK_ICON_SIZE_MENU);
     icon = g_themed_icon_new_with_default_fallbacks ("media-eject");
     icon_info = gtk_icon_theme_lookup_by_gicon (icon_theme, icon, icon_size, 0);
 
