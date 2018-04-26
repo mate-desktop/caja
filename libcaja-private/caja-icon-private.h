99cf0cad9fc6ceb0f86066e85af242d2f46c3b88
Joanmarie Diggs <jdiggs@igalia.com>
2018-04-26 16:13:50 -0400

    Eliminate accessible children-changed event flood on container repopulation
    When the icon container is repopulated (e.g. when using back/forward)
    finish_adding_new_icons is called leading to an accessible children-
    changed event flood. We can distinguish these unwanted notifications
    from desired notifications by comparing the number of new icons to add
    with the size of the container's icon set.

--- libcaja-private/caja-icon-private.h
+++ libcaja-private/caja-icon-private.h
@@ -271,6 +271,7 @@ struct CajaIconContainerDetails
     GQueue* a11y_item_action_queue;
 
     eel_boolean_bit is_loading : 1;
+    eel_boolean_bit is_populating_container : 1;
 
     eel_boolean_bit store_layout_timestamps : 1;
     eel_boolean_bit store_layout_timestamps_when_finishing_new_icons : 1;
