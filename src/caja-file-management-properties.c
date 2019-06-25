51ca975cdb2d9c450275446aeb3e1c39f5f98bad
Laurent Napias <tamplan@free.fr>
2019-06-25 22:15:33 +0200

    Move function to libcaja-private

--- src/caja-file-management-properties.c
+++ src/caja-file-management-properties.c
@@ -40,6 +40,7 @@
 #include <libcaja-private/caja-autorun.h>
 
 #include <libcaja-extension/caja-configurable.h>
+#include <libcaja-private/caja-dialog-notebook.h>
 
 #include "caja-file-management-properties.h"
 
@@ -193,66 +194,6 @@ enum
 };
 
 static void caja_file_management_properties_dialog_update_media_sensitivity (GtkBuilder *builder);
-
-static gboolean
-dialog_page_scroll_event_cb (GtkWidget *widget, GdkEventScroll *event, GtkWindow *window)
-{
-    GtkNotebook *notebook = GTK_NOTEBOOK (widget);
-    GtkWidget *child, *event_widget, *action_widget;
-
-    child = gtk_notebook_get_nth_page (notebook, gtk_notebook_get_current_page (notebook));
-    if (child == NULL)
-        return FALSE;
-
-    event_widget = gtk_get_event_widget ((GdkEvent *) event);
-
-    /* Ignore scroll events from the content of the page */
-    if (event_widget == NULL ||
-        event_widget == child ||
-        gtk_widget_is_ancestor (event_widget, child))
-        return FALSE;
-
-    /* And also from the action widgets */
-    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_START);
-    if (event_widget == action_widget ||
-        (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
-        return FALSE;
-    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_END);
-    if (event_widget == action_widget ||
-        (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
-        return FALSE;
-
-    switch (event->direction) {
-    case GDK_SCROLL_RIGHT:
-    case GDK_SCROLL_DOWN:
-        gtk_notebook_next_page (notebook);
-        break;
-    case GDK_SCROLL_LEFT:
-    case GDK_SCROLL_UP:
-        gtk_notebook_prev_page (notebook);
-        break;
-    case GDK_SCROLL_SMOOTH:
-        switch (gtk_notebook_get_tab_pos (notebook)) {
-            case GTK_POS_LEFT:
-            case GTK_POS_RIGHT:
-                if (event->delta_y > 0)
-                    gtk_notebook_next_page (notebook);
-                else if (event->delta_y < 0)
-                    gtk_notebook_prev_page (notebook);
-                break;
-            case GTK_POS_TOP:
-            case GTK_POS_BOTTOM:
-                if (event->delta_x > 0)
-                    gtk_notebook_next_page (notebook);
-                else if (event->delta_x < 0)
-                    gtk_notebook_prev_page (notebook);
-                break;
-            }
-        break;
-    }
-
-    return TRUE;
-}
 
 static void
 caja_file_management_properties_size_group_create (GtkBuilder *builder,
--- src/file-manager/fm-properties-window.c
+++ src/file-manager/fm-properties-window.c
@@ -58,6 +58,7 @@
 #include <libcaja-private/caja-metadata.h>
 #include <libcaja-private/caja-module.h>
 #include <libcaja-private/caja-mime-actions.h>
+#include <libcaja-private/caja-dialog-notebook.h>
 
 #include "fm-properties-window.h"
 #include "fm-ditem-page.h"
@@ -235,71 +236,7 @@ static GtkLabel *attach_ellipsizing_value_label   (GtkGrid *grid,
 
 static GtkWidget* create_pie_widget 		  (FMPropertiesWindow *window);
 
-static gboolean dialog_page_scroll_event_cb       (GtkWidget          *widget,
-                           GdkEventScroll     *event,
-                           GtkWindow          *window);
-
 G_DEFINE_TYPE_WITH_PRIVATE (FMPropertiesWindow, fm_properties_window, GTK_TYPE_DIALOG);
-
-static gboolean
-dialog_page_scroll_event_cb (GtkWidget *widget, GdkEventScroll *event, GtkWindow *window)
-{
-    GtkNotebook *notebook = GTK_NOTEBOOK (widget);
-    GtkWidget *child, *event_widget, *action_widget;
-
-    child = gtk_notebook_get_nth_page (notebook, gtk_notebook_get_current_page (notebook));
-    if (child == NULL)
-        return FALSE;
-
-    event_widget = gtk_get_event_widget ((GdkEvent *) event);
-
-    /* Ignore scroll events from the content of the page */
-    if (event_widget == NULL ||
-        event_widget == child ||
-        gtk_widget_is_ancestor (event_widget, child))
-        return FALSE;
-
-    /* And also from the action widgets */
-    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_START);
-    if (event_widget == action_widget ||
-        (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
-        return FALSE;
-    action_widget = gtk_notebook_get_action_widget (notebook, GTK_PACK_END);
-    if (event_widget == action_widget ||
-        (action_widget != NULL && gtk_widget_is_ancestor (event_widget, action_widget)))
-        return FALSE;
-
-    switch (event->direction) {
-    case GDK_SCROLL_RIGHT:
-    case GDK_SCROLL_DOWN:
-        gtk_notebook_next_page (notebook);
-        break;
-    case GDK_SCROLL_LEFT:
-    case GDK_SCROLL_UP:
-        gtk_notebook_prev_page (notebook);
-        break;
-    case GDK_SCROLL_SMOOTH:
-        switch (gtk_notebook_get_tab_pos (notebook)) {
-            case GTK_POS_LEFT:
-            case GTK_POS_RIGHT:
-                if (event->delta_y > 0)
-                    gtk_notebook_next_page (notebook);
-                else if (event->delta_y < 0)
-                    gtk_notebook_prev_page (notebook);
-                break;
-            case GTK_POS_TOP:
-            case GTK_POS_BOTTOM:
-                if (event->delta_x > 0)
-                    gtk_notebook_next_page (notebook);
-                else if (event->delta_x < 0)
-                    gtk_notebook_prev_page (notebook);
-                break;
-            }
-        break;
-    }
-
-    return TRUE;
-}
 
 static gboolean
 is_multi_file_window (FMPropertiesWindow *window)
