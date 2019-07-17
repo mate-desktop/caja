3fb05765664fd045997a9827c3ba9598a8962a1c
raveit65 <mate@raveit.de>
2019-07-17 11:11:45 +0200

    fix build error when gla11y is not installed

--- gla11y.mk
+++ gla11y.mk
@@ -10,7 +10,9 @@ a11y_verbose_1 =
 
 all-local: $(GLA11Y_OUTPUT)
 $(GLA11Y_OUTPUT): $(ui_files)
-	$(a11y_verbose) python3 $(GLA11Y) -P $(srcdir)/ -f $(srcdir)/$(GLA11Y_FALSE) -s $(srcdir)/$(GLA11Y_SUPPR) -o $@ $(ui_files:%=$(srcdir)/%)
+	$(a11y_verbose) if [[ $(GLA11Y) == *gla11y ]]; then \
+	        python3 $(GLA11Y) -P $(srcdir)/ -f $(srcdir)/$(GLA11Y_FALSE) -s $(srcdir)/$(GLA11Y_SUPPR) -o $@ $(ui_files:%=$(srcdir)/%); \
+	        fi
 
 clean-local: clean-local-check
 clean-local-check:
