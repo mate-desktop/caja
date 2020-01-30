4832c00beefb4fc7d4ca3a23538941001d28f871
rbuj <robert.buj@gmail.com>
2020-01-30 10:56:12 +0100

    Fix build using gcc 10 -fno-common flag

--- libcaja-private/caja-global-preferences.c
+++ libcaja-private/caja-global-preferences.c
@@ -34,6 +34,19 @@
 #include <eel/eel-string.h>
 #include <glib/gi18n.h>
 
+GSettings *caja_preferences;
+GSettings *caja_media_preferences;
+GSettings *caja_window_state;
+GSettings *caja_icon_view_preferences;
+GSettings *caja_desktop_preferences;
+GSettings *caja_tree_sidebar_preferences;
+GSettings *caja_compact_view_preferences;
+GSettings *caja_list_view_preferences;
+GSettings *caja_extension_preferences;
+
+GSettings *mate_background_preferences;
+GSettings *mate_lockdown_preferences;
+
 /*
  * Public functions
  */
