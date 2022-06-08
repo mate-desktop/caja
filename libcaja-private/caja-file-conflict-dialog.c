ce26516d2a9966f1d1fb7f10882e4ef1ea2f4404
Luke from DC <lukefromdc@hushmail.com>
2022-06-08 00:43:02 +0000

    Revert "caja-file-conflict-dialog: update the icons once"
    This reverts commit c389503d0b9ce61aefb178c16baea12f8b62d49b.

--- libcaja-private/caja-file-conflict-dialog.c
+++ libcaja-private/caja-file-conflict-dialog.c
@@ -68,18 +68,27 @@ G_DEFINE_TYPE_WITH_PRIVATE (CajaFileConflictDialog,
                             GTK_TYPE_DIALOG);
 
 static void
-file_icons_changed (CajaFile  *file,
-                    GtkWidget *widget)
+file_icons_changed (CajaFile *file,
+                    CajaFileConflictDialog *fcd)
 {
     cairo_surface_t *surface;
 
-    surface = caja_file_get_icon_surface (file,
+    surface = caja_file_get_icon_surface (fcd->details->destination,
                                           CAJA_ICON_SIZE_LARGE,
                                           FALSE,
-                                          gtk_widget_get_scale_factor (widget),
+                                          gtk_widget_get_scale_factor (fcd->details->dest_image),
                                           CAJA_FILE_ICON_FLAGS_USE_THUMBNAILS);
 
-    gtk_image_set_from_surface (GTK_IMAGE (widget), surface);
+    gtk_image_set_from_surface (GTK_IMAGE (fcd->details->dest_image), surface);
+    cairo_surface_destroy (surface);
+
+    surface = caja_file_get_icon_surface (fcd->details->source,
+                                          CAJA_ICON_SIZE_LARGE,
+                                          FALSE,
+                                          gtk_widget_get_scale_factor (fcd->details->src_image),
+                                          CAJA_FILE_ICON_FLAGS_USE_THUMBNAILS);
+
+    gtk_image_set_from_surface (GTK_IMAGE (fcd->details->src_image), surface);
     cairo_surface_destroy (surface);
 }
 
@@ -362,11 +371,9 @@ file_list_ready_cb (GList *files,
     caja_file_monitor_add (dest, fcd, CAJA_FILE_ATTRIBUTES_FOR_ICON);
 
     details->src_handler_id = g_signal_connect (src, "changed",
-                                                G_CALLBACK (file_icons_changed),
-                                                fcd->details->src_image);
+                              G_CALLBACK (file_icons_changed), fcd);
     details->dest_handler_id = g_signal_connect (dest, "changed",
-                                                 G_CALLBACK (file_icons_changed),
-                                                 fcd->details->dest_image);
+                               G_CALLBACK (file_icons_changed), fcd);
 }
 
 static void
